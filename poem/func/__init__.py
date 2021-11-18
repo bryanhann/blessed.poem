7a897888606dc59a25520e91e27297df0b100e0f
Bryan Hann <nobody@nowhere>
2021-11-18 14:48:21 -0330

    Add shabang to script.

--- poem/func/__init__.py
+++ poem/func/__init__.py
@@ -1,3 +1,5 @@
-comp = lambda f2, f1 : lambda *x, **y : f2(f1(*x,**y))
-lmap = comp(list,map)
-lfilter = comp(list,filter)
+#!/usr/bin/env python3
+
+__comp = lambda f2, f1 : lambda *x, **y : f2(f1(*x,**y))
+lmap = __comp(list,map)
+lfilter = __comp(list,filter)
